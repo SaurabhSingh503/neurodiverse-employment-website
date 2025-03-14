4835dae2011fb44a883829e621e44c7d2c635979
SaurabhSingh503 <saurabhsinghstu456@gmail.com>
2025-03-14 13:48:26 +0530

    Updated HTML files for GitHub Pages

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,10 @@
 // Add interactivity here
 document.getElementById('loginForm').addEventListener('submit', function (e) {
-    e.preventDefault();
-    alert('Login functionality coming soon!');
-  });
-  
-  document.getElementById('signupForm').addEventListener('submit', function (e) {
-    e.preventDefault();
-    alert('Signup functionality coming soon!');
-  });
+  e.preventDefault();
+  alert('Login functionality coming soon!');
+});
+
+document.getElementById('signupForm').addEventListener('submit', function (e) {
+  e.preventDefault();
+  alert('Signup functionality coming soon!');
+});
